694ede5082842cf592c24a9b81569cdb8bf6a14b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-17 12:12:42 +0700

    Fix `mini.css` linking error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './styles/index.css'
-import 'mini.css'
+import 'mini.css/dist/mini-dark.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
