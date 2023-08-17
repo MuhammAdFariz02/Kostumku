4934afd183c5df719ecf77a9aede894e63f4dc4b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-17 12:17:35 +0700

    Change wrong theme `mini.css`

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './styles/index.css'
-import 'mini.css/dist/mini-dark.css'
+import 'mini.css/dist/mini-default.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
